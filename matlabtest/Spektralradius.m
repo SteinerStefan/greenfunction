c08c18844d2a097d1537862eeead8bdd81bc14c5
alinggi <alinggi@hsr.ch>
2014-05-05 11:27:02 +0200

    Zweite Matrix A2(10^2 x 10^2) hinzugefügt

--- matlabtest/Spektralradius.m
+++ matlabtest/Spektralradius.m
@@ -15,6 +15,7 @@ end;
 T = T+ eye(n)*-4;
 E = eye(n);
 Z = zeros(n);
+
 A = [T E Z Z Z;
      E T E Z Z;
      Z E T E Z;
@@ -24,6 +25,37 @@ A = [T E Z Z Z;
 M = tril(A);
 N = A-M;
 C = pinv(M)*N;
-Spektralradius = max(eig(C));
+Spektralradius1 = max(eig(C));
 
-fprintf('Spektralradius: %4.5f\n',Spektralradius);
+
+fprintf('Spektralradius von A(5^2 x 5^2): %4.5f\n',Spektralradius1);
+
+n = 10;        %dimension von f
+T = [];
+for i=1:n-1
+    T(i,i+1)=1;
+end;
+for i=2:n
+    T(i,i-1)=1;
+end;
+T = T+ eye(n)*-4;
+E = eye(n);
+Z = zeros(n);
+
+A2 =[T E Z Z Z Z Z Z Z Z;
+     E T E Z Z Z Z Z Z Z;
+     Z E T E Z Z Z Z Z Z;
+     Z Z E T E Z Z Z Z Z;
+     Z Z Z E T E Z Z Z Z;
+     Z Z Z Z E T E Z Z Z;
+     Z Z Z Z Z E T E Z Z;
+     Z Z Z Z Z Z E T E Z;
+     Z Z Z Z Z Z Z E T E;
+     Z Z Z Z Z Z Z Z E T];
+
+M2 = tril(A2);
+N2 = A2-M2;
+C2 = pinv(M2)*N2;
+Spektralradius2 = max(eig(C2));
+
+fprintf('Spektralradius von A(10^2 x 10^2): %4.5f\n',Spektralradius2);
