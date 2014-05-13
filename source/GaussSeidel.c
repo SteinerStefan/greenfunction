14e69b7c093b618e03d020c1ec6b689286ca3807
Stefan Steiner <stektograph@gmail.com>
2014-05-13 13:17:52 +0200

    parallelisiert

--- source/GaussSeidel.c
+++ source/GaussSeidel.c
@@ -40,10 +40,11 @@ void gaussSeidel(float* f, float* x, int n, int iter, int numthreads, char* data
 	freeMatrix(outImage,n);                  		    
 	
 
+
 	for(int step = 1; step <= iter; step++)	
 	{
-		if(step == 1)
-		{
+		//if(step == 1)
+		//{
 			//outImage = newMatrixFromVector(x,n);     // Matrix aus Vector erstellen
 			//writeMatrixAsCSVtoFile(outImage, n, step, dataFoldName);  // in csv File scheiben
 			//freeMatrix(outImage,n);                  		    
@@ -53,45 +54,30 @@ void gaussSeidel(float* f, float* x, int n, int iter, int numthreads, char* data
 			//printf("%4.0f %4.0f %4.0f\n",x[testy*n + (testx-1)], x[testy*n + testx], x[(testy)*n + testx]);
 			//printf("     %4.0f\n", x[(testy+1)*n + testx]);
 
-		}
-		//Ecken
-		x[0] = (f[0] - x[1] - x[n])/-tf;         //links oben
-		x[n-1] = (f[n-1] - x[n-2] - x[2*n-1])/-tf; //rechts oben
-		x[n2- n] =(f[n2-1-n] - x[n2-n] - x[n2-2*n-1] )/-tf; //links unten
-		x[n2-1] = (f[n2-1]  - x[n2-2] - x[n2-n-1])/-tf; //rechts unten
-
-		//Rand
+		//}
 		#pragma omp parallel for num_threads(numthreads)
-		for(int l = 1; l<(n-1); l++)//oben
-			x[l] = (f[l] - x[l+1] - x[l-1] -x[l+n])/-tf;
-		#pragma omp parallel for num_threads(numthreads)
-		for(int l = n2-n+1; l<n2-1; l++)//unten
-			x[l] = (f[l] - x[l+1] - x[l-1] - x[l-n])/-tf;
-		#pragma omp parallel for num_threads(numthreads)
-		for(int l = n; l<=(n2-2*n); l+=n)//links
-			x[l] = (f[l] - x[l+1] - x[l-n] - x[l+n])/-tf;
-		#pragma omp parallel for num_threads(numthreads)	
-		for(int l = (2*n-1); l<= (n2-n-1); l+=n)//rechts
-			x[l] = (f[l]  - x[l-1] - x[l-n] - x[l+n])/-tf;
-
-		//Rest
-		#pragma omp parallel for num_threads(numthreads)
-		for(int u = n+1; u <(n2-n-2); u+=n)
-		{
-			for(int l = u;l < u+n-2; l++) 
-			{		
-				//if(x!=0) schneller???
-				x[l] = (f[l] - x[l+1] - x[l-1] - x[l-n] -x[l+n])/-tf;
+		for(int u = 0; u < n2; u++)
+		{	
+			if(u < n || u > n2-n || u % n == 0 || u %n == n-1)
+			{
+			   if(u > 0 && u < n-1)                             x[u]    = (f[u]      - x[u+1] - x[u-1] -x[u+n]) /-tf;//oben
+			   else if(u > n2-n && u <n2-1)                     x[u]    = (f[u]      - x[u+1] - x[u-1] - x[u-n])/-tf;  //unten
+			   else if(u % n == 0 && u >= n && u <= (n2-2*n))   x[u]    = (f[u]      - x[u+1] - x[u-n] - x[u+n])/-tf;   //links
+			   else if(u %n == n-1 && u >=2*n-1 && u <= n2-n-1) x[u]    = (f[u]      - x[u-1] - x[u-n] - x[u+n])/-tf;//rechts
+			   else if(u == 0)                                  x[0]    = (f[0]      - x[1]   - x[n])           /-tf; //links oben
+			   else if(u == n2-n)                               x[n2-n] = (f[n2-1-n] - x[n2-n]- x[n2-2*n-1] )   /-tf;//links unten
+			   else if(u == n-1)                                x[n-1]  = (f[n-1]    - x[n-2] - x[2*n-1])       /-tf; // rechts oben
+			   else                                             x[n2-1] = (f[n2-1]   - x[n2-2]- x[n2-n-1])      /-tf;// rechts unten
 			}
+			else x[u] = (f[u] - x[u+1] - x[u-1] - x[u-n] -x[u+n])/-tf;//punkt ist nicht am rand!!
 		}
-
 		//for(int u = 0; u <n2; u++)x[u] = help[u];
 			
-			printf("\nIteration %3d of %3d: Give out f[%d, %d] = %f \n",step,iter, testx,testy,f[testx*n+testy]); 
-			printf("     %4.0f\n", x[(testy-1)*n + testx]);
-			printf("%4.0f %4.0f %4.0f\n",x[testy*n + (testx-1)], x[testy*n + testx], x[(testy)*n + testx]);
-			printf("     %4.0f\n", x[(testy+1)*n + testx]);
-			printf("write to csv\n");
+			printf("\nIteration %3d of %3d: Give out f[%d, %d] = %f ",step,iter, testx,testy,f[testx*n+testy]); 
+			//printf("     %4.0f\n", x[(testy-1)*n + testx]);
+			//printf("%4.0f %4.0f %4.0f\n",x[testy*n + (testx-1)], x[testy*n + testx], x[(testy)*n + testx]);
+			//printf("     %4.0f\n", x[(testy+1)*n + testx]);
+			//printf("write to csv\n");
 			outImage = newMatrixFromVector(x,n);     // Matrix aus Vector erstellen
 			writeMatrixAsCSVtoFile(outImage, n, step, dataFoldName);  // in csv File scheiben
 			freeMatrix(outImage,n);                  		    
